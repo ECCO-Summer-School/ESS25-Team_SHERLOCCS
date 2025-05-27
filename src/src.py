9ce40c0d3e1cfe4fbd4f62243ae0554e139d1566
Anthony Meza <ameza98@outlook.com>
2025-05-27 21:51:55 +0000

    updating some heat budget notebooks

--- src/src.py
+++ src/src.py
@@ -1,3 +1,43 @@
+# tell Python to use the ecco_v4_py in the 'ECCOv4-py' repository
+from os.path import join,expanduser
+import sys
+
+# identify user's home directory
+user_home_dir = expanduser('~')
+
+# import the ECCOv4 py library 
+sys.path.insert(0,join(user_home_dir,'ECCOv4-py'))
+
+import botocore  
+import boto3
+from boto3.session import Session
+import cmocean
+from collections import Counter
+from dask.distributed import Client
+import datetime
+import fsspec
+from gc import get_referents
+import json
+import numpy as np
+from pathlib import Path
+from pprint import pprint
+import requests
+import s3fs
+import sys
+from sys import getsizeof
+import time as time
+from types import ModuleType, FunctionType
+import xarray as xr
+import matplotlib.pyplot as plt
+import zarr
+import xgcm
+import ecco_v4_py as ecco
+import ecco_v4_py.ecco_access as ea
+import fsspec
+import zarr
+
+from dask.distributed import Client
+
 from finite_differences import * 
 from grid_utils import * 
 from heat_budgets import * 
